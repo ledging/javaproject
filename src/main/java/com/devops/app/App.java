700bb920d20811f652f2b654ba002631604bcede
venkat0007 <vennavenkat7@gmail.com>
2017-12-04 22:25:36 +0530

    Update App.java

--- src/main/java/com/devops/app/App.java
+++ src/main/java/com/devops/app/App.java
@@ -13,9 +13,10 @@ public class App
         System.out.println( "Transaction method!" );
         System.out.println( "OTP method!" );
         System.out.println( "OTP method-2!" );
-		System.out.println( "BankStatement in PDF report!" );
-		System.out.println( "IMPS method!" );
-		System.out.println( "NEFT method!" );
-		System.out.println( "NEFT method-2!" );
+	System.out.println( "BankStatement in PDF report!" );
+	System.out.println( "IMPS method!" );
+	System.out.println( "NEFT method!" );
+	System.out.println( "NEFT method-2!" );
+    	System.out.println( "Bill payments!" );
     }
 }
